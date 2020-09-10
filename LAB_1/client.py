e1a20c90af2c9a6273a68c863faf29fa582c9db9
ismailfaruk <ismailfaruk.17@gmail.com>
2020-09-10 14:24:56 -0400

    client and server file started

---
+++ LAB_1/client.py
@@ -0,0 +1,5 @@
+# this is the client file
+
+
+if __name__ == "__main__":
+    print("I am client")
---
+++ LAB_1/server.py
@@ -0,0 +1,5 @@
+# this is the server file
+
+
+if __name__ == "__main__":
+    print("I am server")
