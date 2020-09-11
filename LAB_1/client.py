86b5f1fd086c9e4f719fbf5b1cf3e6dbdac89c8c
Kevin Chuong <kevin.chuong@mail.mcgill.ca>
2020-09-10 21:43:12 -0400

    Basic client that sends msg from client socket to server socket.

--- LAB_1/client.py
+++ LAB_1/client.py
@@ -1,5 +1,26 @@
-# this is the client file
+import socket
+import argparse
+
+# TODO: Uncomment this once HTTP request + opening files on server is done
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('host', type=str)
+# parser.add_argument('port', type=int)
+# parser.add_argument('filename', type=str)
+# parser.add_argument('timeout', type=int, nargs='?', default=5)
+
+# args = parser.parse_args()
+
+# print(args.host)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# client usually remote to server, but in this case its on the same machine
+client_socket.connect((socket.gethostname(), 1337))
 
 
-if __name__ == "__main__":
-    print("I am client")
+client_socket.send(bytes("Successfully Connected!", "utf-8"))
+
+# TODO
+# 1. Set up client_socket with argument values
+# 2. Print file content when  HTTP response received, otherwise terminate after <-timeout>
--- LAB_1/server.py
+++ LAB_1/server.py
@@ -1,5 +1,35 @@
-# this is the server file
+import socket
 
+# AF_INET corresponds to IPV4 sockets
+# SOCK_STREAM corresponds to TCP 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == "__main__":
-    print("I am server")
+# socket.gethosename makes socket visible to outside world.
+server_socket.bind((socket.gethostname(), 1337))
+server_socket.settimeout(1) # Set timeout to 1 second so accept() will become (somewhat nonblocking)
+# Socket is simply an endpoint that send/receives data.
+# It sits in a corresponding port-IP pair
+
+# Prepare to listen to a single socket at most
+server_socket.listen(1)
+
+try:
+    while True:
+        # create connection socket, address -> IP address of where data coming from
+        try:
+            (client_socket, address) = server_socket.accept() # Note that accept here is blocking 
+            # Remember sockets receive stream of data ( how much chunks of data we receive at a time)
+            msg = client_socket.recv(1024) # MAX of 1024 bytes for the buffer
+            print(msg.decode("utf-8"))
+
+        except socket.timeout:
+            pass
+
+        # TODO
+        # 1. Receive an HTTP request
+        # 2. Parse it to retrieve filename
+        # 3. Get file from directory
+        # 4. Send HTTP response to client with file content (404 if file not found)
+
+except KeyboardInterrupt:
+    server_socket.close()
