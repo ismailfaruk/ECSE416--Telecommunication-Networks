30a3977ff256741e82a3a86325a17da27b9e6bef
ismailfaruk <ismailfaruk.17@gmail.com>
2020-09-13 18:22:03 -0400

    revert commit on client file

--- LAB_1/client.py
+++ LAB_1/client.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
 
     # TODO: Uncomment this once HTTP request + opening files on server is done
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('host', type=str)
-    parser.add_argument('port', type=int)
-    parser.add_argument('filename', type=str)
-    parser.add_argument('timeout', type=int, nargs='?', default=5)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('host', type=str)
+    # parser.add_argument('port', type=int)
+    # parser.add_argument('filename', type=str)
+    # parser.add_argument('timeout', type=int, nargs='?', default=5)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    print(args.host)
+    # print(args.host)
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # client usually remote to server, but in this case its on the same machine
     client_socket.connect((socket.gethostname(), Port))
