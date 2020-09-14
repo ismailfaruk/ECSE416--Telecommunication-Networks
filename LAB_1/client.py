bb231338265d7d85769e3887437422a58263ed8b
ismailfaruk <ismailfaruk.17@gmail.com>
2020-09-13 20:44:45 -0400

    implemented generic client print outs

--- LAB_1/client.py
+++ LAB_1/client.py
@@ -1,31 +1,50 @@
 import socket
 import argparse
+import sockethelp
 
-# Client Definitions
-Port = 1337         # low number ports are usually reserved, use a nice high number (4 digits) https://docs.python.org/3.5/howto/sockets.html
+#----------------------------------------Client Definitions----------------------------------------
+Host = socket.gethostname()     # Sets the current computer name as hostname
+Port = 1337                     # low number ports are usually reserved, use a nice high number (4 digits) https://docs.python.org/3.5/howto/sockets.html
+BufferSize = 1024               # MAX of 1024 bytes for the buffer
+EncodeFormat = "utf-8"          # Message encoder format
+Timeout = 5                     # Set timeout to 1 second so accept() will become (somewhat nonblocking)
+#--------------------------------------------------------------------------------------------------
 
-if __name__ == "__main__":
-
+def interface():
     # TODO: Uncomment this once HTTP request + opening files on server is done
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('host', type=str)
-    # parser.add_argument('port', type=int)
-    # parser.add_argument('filename', type=str)
-    # parser.add_argument('timeout', type=int, nargs='?', default=5)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', type=str)
+    parser.add_argument('port', type=int)
+    parser.add_argument('filename', type=str)
+    parser.add_argument('timeout', type=int, nargs='?', default=5)
+    args = parser.parse_args()
+    print(args.host)
+    return args
 
-    # args = parser.parse_args()
+def client(Host, Port, Filename, Timeout):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((Host, Port))
+        print("Connection: OK")        
 
-    # print(args.host)
+        sockethelp.write(client_socket, Filename)
+        print("Request message sent.")
 
-    # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ServerMessage = sockethelp.read(client_socket)
+        print("Server HTTP Response: ", ServerMessage)
+        
+        client_socket.close()
+        print("Socket Closed.")
 
-    # client usually remote to server, but in this case its on the same machine
-    client_socket.connect((socket.gethostname(), Port))
-
-
-    client_socket.send(bytes("Successfully Connected!", "utf-8"))
+    except Exception as ErrorMessage:
+        print("- ERROR: ", ErrorMessage)
 
     # TODO
     # 1. Set up client_socket with argument values
     # 2. Print file content when  HTTP response received, otherwise terminate after <-timeout>
+
+if __name__ == "__main__":
+    # Input = interface()
+    # client(Input.host, Input.port, Input.filename, Input.timeout)
+    client(Host, Port, "pic.jpg", Timeout)
